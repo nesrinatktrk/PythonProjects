4049dc29e8684387b9590a5ce1e53fa2e5815401
Nesrin Atiktürk <79600556+nesrinatktrk@users.noreply.github.com>
2022-08-10 14:48:14 +0300

    Add files via upload

---
+++ ipbul_uyg.py
@@ -0,0 +1,23 @@
+import socket
+
+def ipBul():
+    benimip=(socket.gethostbyname(socket.gethostname()))  
+    print(benimip)
+        with open("ip.txt","wb") as ipDosyam: 
+            ipDosyam.write(benimip)
+ipBul()
+
+mevcutip=(socket.gethostbyname(socket.gethostname()))
+
+def dosyaAc(dosya):
+    with open(dosya,"rb") as dosyam:
+        return dosyam.read() 
+
+benimip2= dosyaAc("ip.txt")
+print(benimip)
+print(mevcutip)
+
+if benimip2 == mevcutip:
+    print("sorun yok")
+else:
+    print("kontrol başlatıldı")
